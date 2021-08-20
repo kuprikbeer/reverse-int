316ec2906a7bd3bc8b02f31591add0aab4fe1487
Zhenya <kuprikbeer@gmail.com>
2021-08-20 21:56:13 +0300

    fix:fix bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function reverse(n) {
     if (n < 0) {
         n = 0 - n
-    } npm
+    }
     let str = JSON.stringify(n)
     let rev = str.split('').reverse().join('')
     return rev * 1
